545ac03cd84c9acc9012c37cd1be2fc61cf0f1d0
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-09-18 10:22:25 +0200

    Clean rollup config

--- packages/frontend/rollup.config.js
+++ packages/frontend/rollup.config.js
@@ -1,152 +1,136 @@
-import resolve from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
-import commonjs from '@rollup/plugin-commonjs'
-import svelte from 'rollup-plugin-svelte'
-import babel from '@rollup/plugin-babel'
-import { terser } from 'rollup-plugin-terser'
-import config from 'sapper/config/rollup.js'
-import pkg from './package.json'
-import scsss from 'rollup-plugin-scss'
-const auto = require('svelte-preprocess')
-const rootImport = require('rollup-plugin-root-import')
-const autoprefixer = require('autoprefixer')
-import { scss, postcss } from 'svelte-preprocess'
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
+import _scss from 'rollup-plugin-scss';
+import { scss as autoscss, postcss as autopostcss } from 'svelte-preprocess';
 
-import { mdsvex } from 'mdsvex'
-
-const mode = process.env.NODE_ENV
-const dev = mode === 'development'
-const legacy = !!process.env.SAPPER_LEGACY_BUILD
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning)
+  (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+  (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning);
+
+const scss = {
+  includePaths: ['src/styles'],
+  output: 'static/global.css',
+};
 
 const preprocess = [
-	mdsvex(),
-	scss({
-		includePaths: ['src/styles'],
-		data: `@import 'src/styles/variables.scss';`
-	}),
-	postcss({
-		plugins: [require('autoprefixer')]
-	})
-]
+  autoscss({
+    includePaths: ['src/styles'],
+    data: `@import 'src/styles/variables.scss';`,
+  }),
+  autopostcss({
+    plugins: [require('autoprefixer')],
+  }),
+];
 
 export default {
-	client: {
-		input: config.client.input(),
-		output: config.client.output(),
-		plugins: [
-			scsss({
-				// update includePaths to what suits.
-				// node_modules is probably only necessary if you need to import from a css library
-				includePaths: ['src/styles'],
-				output: 'static/global.css'
-			}),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				dev,
-				extensions: ['.svelte', '.svx'],
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      _scss(scss),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      svelte({
+        dev,
+        hydratable: true,
+        emitCss: true,
+        preprocess,
+      }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte'],
+      }),
+      commonjs(),
 
-				hydratable: true,
-				emitCss: true,
-				preprocess
-			}),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
+      legacy &&
+        babel({
+          extensions: ['.js', '.mjs', '.html', '.svelte'],
+          babelHelpers: 'runtime',
+          exclude: ['node_modules/@babel/**'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: '> 0.25%, not dead',
+              },
+            ],
+          ],
+          plugins: [
+            '@babel/plugin-syntax-dynamic-import',
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                useESModules: true,
+              },
+            ],
+          ],
+        }),
 
-			legacy &&
-				babel({
-					extensions: ['.js', '.mjs', '.html', '.svelte'],
-					babelHelpers: 'runtime',
-					exclude: ['node_modules/@babel/**'],
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								targets: '> 0.25%, not dead'
-							}
-						]
-					],
-					plugins: [
-						'@babel/plugin-syntax-dynamic-import',
-						[
-							'@babel/plugin-transform-runtime',
-							{
-								useESModules: true
-							}
-						]
-					]
-				}),
+      !dev &&
+        terser({
+          module: true,
+        }),
+    ],
 
-			!dev &&
-				terser({
-					module: true
-				})
-		],
+    preserveEntrySignatures: false,
+    onwarn,
+  },
 
-		preserveEntrySignatures: false,
-		onwarn
-	},
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      _scss(scss),
+      replace({
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      svelte({
+        generate: 'ssr',
+        dev,
+        hydratable: true,
+        hydratable: true,
+        emitCss: true,
+        preprocess,
+      }),
+      resolve({
+        dedupe: ['svelte'],
+      }),
+      commonjs(),
+    ],
+    external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
 
-	server: {
-		input: config.server.input(),
-		output: config.server.output(),
-		plugins: [
-			scsss({
-				// update includePaths to what suits.
-				// node_modules is probably only necessary if you need to import from a css library
-				includePaths: ['src/styles'],
-				output: 'static/global.css'
-			}),
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				generate: 'ssr',
-				hydratable: true,
-				extensions: ['.svelte', '.svx'],
+    preserveEntrySignatures: 'strict',
+    onwarn,
+  },
 
-				dev,
-				preprocess,
+  serviceworker: {
+    input: config.serviceworker.input(),
+    output: config.serviceworker.output(),
+    plugins: [
+      resolve(),
+      replace({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+      }),
+      commonjs(),
+      !dev && terser(),
+    ],
 
-				hydratable: true,
-				emitCss: true
-			}),
-			resolve({
-				dedupe: ['svelte']
-			}),
-			commonjs()
-		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
-
-		preserveEntrySignatures: 'strict',
-		onwarn
-	},
-
-	serviceworker: {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-			!dev && terser()
-		],
-
-		preserveEntrySignatures: false,
-		onwarn
-	}
-}
+    preserveEntrySignatures: false,
+    onwarn,
+  },
+};
