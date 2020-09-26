a716347ed97958e623125925179c23333a4c7ed4
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-09-26 18:45:43 +0200

    Redeploy Rinkeby contracts

--- packages/contracts/deploy/3 - Whitelist.js
+++ packages/contracts/deploy/3 - Whitelist.js
@@ -5,7 +5,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deployer } = await getNamedAccounts();
 
   const presale = await API3Presale.at((await get('API3Presale')).address);
-  await presale.whitelist(['0xb71D2D88030A00830C3D45f84C12cc8aAF6857a5'], { from: deployer });
+  await presale.whitelist(['0xb71D2D88030A00830C3D45f84C12cc8aAF6857a5', '0xbEC82ea560a37d1159B09ac80489ea4B0A4e41F1'], { from: deployer });
 
   log('+ Whitelisted 0xb71D2D88030A00830C3D45f84C12cc8aAF6857a5');
 };
